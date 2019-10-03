221ecbf4a0b534f0fb8a02980efcd8b55e994f28
michealwave <56037909+michealwave@users.noreply.github.com>
2019-10-03 09:04:40 -0700

    Add files via upload
    updated

--- dice.py
+++ dice.py
@@ -1,9 +1,27 @@
+# Michael Williams
+# Period 1
+# 10/3/19
 import random
 
-while True:
-	roll = int(input("How many rolls would you like? "))
+one = 0
+two = 0
+three = 0
+four = 0
+five = 0
+six = 0
+rollNum = 1 
+rolls = int(input("How many rolls would you like? "))
 
-	print(random.randint(1, 6) ) 
+while True:
+	randnum = random.randint(1, 6)
+	print("Roll #" + str(rollNum) + str(randnum))
+
+	if 
+
+	
+
+
+
+
 
 		
-
