4c5d04ffab325b7a670933224b6596197f94bd72
michealwave <56037909+michealwave@users.noreply.github.com>
2019-10-22 09:04:02 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,28 +1,38 @@
 myword = "shrimp"
 #how to check if a letter is in a word
-mylist = [" ", " ", " ", " ", " ", " "]
-print(mylist)
+mylist = list(myword)
+guesslist = []
+turns = 10
+miss = []
 
-letter = input("Enter a letter: ")
-if letter in myword:
+for letter in mylist:
+	guesslist.append("_") 
+print(guesslist)
+
+for letter2 in miss:
+	guesslist.append("_")
+print(miss)
+while True:
+	letter = input("Enter a letter: ")
 	if letter == "s":
-		mylist.insert(0, "s")
+		guesslist[0] = "s"
+		
 	elif letter == "h":
-		mylist.insert(1, "h")
+		guesslist[1] = "h"
+		
 	elif letter == "r":
-		mylist.insert(2, "r")
+		guesslist[2] = "r"
+		
 	elif letter == "i":
-		mylist.insert(3, "i")
+		guesslist[3] = "i"
+		
 	elif letter == "m":
-		mylist.insert(4, "m")
-	elif letter == "p"
-	mylist.insert(5, "p")
-else:
-	print("nah")
+		guesslist[4] = "m"
+		
+	elif letter == "p":
+		guesslist[5] = "p"
+	else:
+		miss.append(letter)
+	print(guesslist)	
+	print(miss)
 
-count = 0
-for letter2 in mylist:
-	if letter2 == letter:
-		print(count)
-	count += 1
-print(mylist)
--- hints.py
+++ hints.py
@@ -23,3 +23,39 @@ for letter2 in mylist:
 	if letter2 == letter:
 		print(count)
 	count += 1
+
+
+myString = "Arizona"
+mysteryword = list(myString)
+print(mysteryword)
+guesslist = []
+#How to make a list with _ for characters
+
+for letter in mysteryword:
+	guesslist.append("_")
+
+print(guesslist)
+
+#how to replace a specific index in a list
+guesslist[3] = "z"
+print(guesslist)
+#--------------------------------------------------------------
+if letter == "s":
+		guesslist[0] = "s"
+		print(guesslist)
+	elif letter == "h":
+		guesslist[1] = "h"
+		print(guesslist)
+	elif letter == "r":
+		guesslist[2] = "r"
+		print(guesslist)
+	elif letter == "i":
+		guesslist[3] = "i"
+		print(guesslist)
+	elif letter == "m"
+	guesslist[4] = "m"
+	print(guesslist)
+	elif letter == "p"
+	guesslist[4] = "p"
+	print(guesslist)
+#
