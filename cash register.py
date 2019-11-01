bf47d455207fdfbaac11190721d9458f5f6ecb12
michealwave <56037909+michealwave@users.noreply.github.com>
2019-11-01 09:05:05 -0700

    Add files via upload

--- cash register.py
+++ cash register.py
@@ -9,8 +9,35 @@ quarter = 0
 dime = 0
 nickels = 0
 pennies = 0
+price = 0
+given = 0
 
-price = int(input("What is the price of the item? "))
-given = int(input("How much are you paying with? "))
+price = float(input("What is the price of the item? "))
+given = float(input("How much are you paying with? "))
+
+change = given - price
+change = change * 100
+
+dollars = int(change / 100)
+change = change % 100
+
+halfdollar = int(change / 50)
+change = change % 50
+
+quarter = int(change / 25)
+change = change % 25
+
+dime = int(change / 10)
+change = change % 10
+
+nickels = int(change / 5)
+change = change % 5
 
 
+print(change)
+print(dollars)
+print(halfdollar)
+print(quarter)
+print(dime)
+print(nickels)
+print("Change: " + change)
