4875c01b395e4e7913f0bd75bded1513e30f7ca5
michealwave <56037909+michealwave@users.noreply.github.com>
2020-10-13 20:13:46 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    int mynumber = 12;
+    mynumber = mynumber/5;
+    System.out.println(mynumber);
+     int myNumber; // creates an int variable calls it myname
+     myNumber = 14; // assigns myNumber to 14
+     // the first time you assign a value its called initializing
+     final int MYNUM = 17; // the word final means the varible is constant
+
+
+  }
+  
+
+}
